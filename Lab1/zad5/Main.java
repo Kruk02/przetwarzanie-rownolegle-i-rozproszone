fbb5f7d90eaf9edc0de34449dc84a328bcc4a36c
Grzegorz Kruk <krukg02@gmail.com>
2026-05-31 14:04:38 +0200

    zadanie5main

---
+++ Lab1/zad5/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Pracownik p1 = new Pracownik("Karol", 19);
+        Pracownik p2 = new Pracownik("Kamil", 24);
+
+        p1.przedstaw_sie();
+        p2.przedstaw_sie();
+    }
+}
