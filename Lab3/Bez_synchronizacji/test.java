71d531bbdcceb2456a130e0877668caedaf61a81
Grzegorz Kruk <krukg02@gmail.com>
2026-05-31 16:00:52 +0200

    Add Test class for running Manipulacja without synchronization

---
+++ Lab3/Bez_synchronizacji/test.java
@@ -0,0 +1,17 @@
+public class Test implements Runnable {
+    private Manipulacja manipulacja;
+    public Test(Manipulacja manipulacja){
+        this.manipulacja = manipulacja;
+    }
+    @Override
+    public void run(){
+        for (int i = 0; i < 1000; i++) {
+            double wynik = manipulacja.wykonaj_bez_synchronizacji();
+
+            if (wynik < 0){
+                System.out.println("Blad : wynik ujemny : " + wynik);
+                return;
+            }
+        }
+    }
+}
